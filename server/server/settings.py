63a3184ac8bae228eed55de4655b0ed70e34ebcd
Natan-Asrat <nathanyilmaasrat@gmail.com>
2026-02-04 23:34:41 +0300

    fixed static issue

--- server/server/settings.py
+++ server/server/settings.py
@@ -160,9 +160,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-PATH_PREFIX = os.environ.get("PATH_PREFIX", "")
 
-STATIC_URL = f"static/"
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
