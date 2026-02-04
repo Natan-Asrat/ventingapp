38e64e4ea7d59b94ad03315d26314c48166586b1
Natan-Asrat <nathanyilmaasrat@gmail.com>
2026-02-04 22:31:59 +0300

    debug

--- server/server/urls.py
+++ server/server/urls.py
@@ -3,16 +3,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path(f"{settings.PATH_PREFIX}/admin/", admin.site.urls),
-    path(f"{settings.PATH_PREFIX}/api/account/", include("account.urls")),
-    path(f"{settings.PATH_PREFIX}/api/post/", include("post.urls")),
-    path(f"{settings.PATH_PREFIX}/api/transaction/", include("transaction.urls")),
-    path(f"{settings.PATH_PREFIX}/api/notification/", include("notification.urls")),
-    path(f"{settings.PATH_PREFIX}/api/report/", include("report.urls")),
-    path(f"{settings.PATH_PREFIX}/api/usage/", include("usage.urls")),
-    path(f"{settings.PATH_PREFIX}/api/chat/", include("conversation.urls")),
-    path(f"{settings.PATH_PREFIX}/api/support/", include("support.urls")),
-    path(f"{settings.PATH_PREFIX}/api/analytics/", include("analytics.urls"))
+    path("admin/", admin.site.urls),
+    path("api/account/", include("account.urls")),
+    path("api/post/", include("post.urls")),
+    path("api/transaction/", include("transaction.urls")),
+    path("api/notification/", include("notification.urls")),
+    path("api/report/", include("report.urls")),
+    path("api/usage/", include("usage.urls")),
+    path("api/chat/", include("conversation.urls")),
+    path("api/support/", include("support.urls")),
+    path("api/analytics/", include("analytics.urls"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
