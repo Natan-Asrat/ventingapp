63c11d4b0289cd5bab7cad262afde440b1e37b73
Natan-Asrat <nathanyilmaasrat@gmail.com>
2026-02-04 22:25:46 +0300

    debugging

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,8 +15,10 @@ urlpatterns = [
     path(f"{settings.PATH_PREFIX}/api/analytics/", include("analytics.urls"))
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     from debug_toolbar.toolbar import debug_toolbar_urls
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += debug_toolbar_urls()
