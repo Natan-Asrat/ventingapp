24a536f62e12d267ee577037994d6b0353dce4b1
Natan-Asrat <nathanyilmaasrat@gmail.com>
2026-02-06 06:54:17 +0300

    test google-analytics

--- client/src/main.js
+++ client/src/main.js
@@ -10,10 +10,18 @@ import vClickOutside from "click-outside-vue3";
 // Import our configured axios instance
 import api from './api/axios';
 
+import VueGtag from "vue-gtag";
+
 // Create app
 const app = createApp(App);
 const pinia = createPinia();
-
+app.use(VueGtag, {
+  config: { 
+    id: "G-3CTKDGJ34R" 
+  },
+  // This automatically tracks page changes as users navigate
+  pageTrackerScreenviewEnabled: true 
+}, router);
 // Make axios available globally (optional)
 app.config.globalProperties.$http = api;
 
